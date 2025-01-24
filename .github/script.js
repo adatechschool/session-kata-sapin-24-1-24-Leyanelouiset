3cc4b82bb284a14d434ba171310a4601766f9834
leyanelouiset <louisetleyane@gmail.com>
2025-01-24 12:31:19 +0100

    1.1

---
+++ .github/script.js
@@ -0,0 +1,28 @@
+//function afficherPointeSapin (hauteur){
+   // console.log(*)
+//if(hauteur<)
+//}
+//afficherPointeSapin()
+
+
+
+function afficherEtoiles(n) {
+    let etoile=''
+for(let i =0;i<n;i++){
+etoile +='*'
+}
+console.log(etoile)
+}
+
+afficherEtoiles(2)
+  afficherEtoiles(5)
+
+
+  function afficherRectangle(hauteur, largeur) {
+    for(let i =0;i<hauteur&& largeur;i++){
+        +='*'
+        }
+  }
+  afficherRectangle(5, 5)
+  
+
